ad5dbc3f17364218acb666fe21323b432ff4edfc
LittleSaltyFish <1134730664@qq.com>
2016-12-14 18:38:40 +0800

    12.14 Web存储-localStroage

---
+++ Web存储-localStroage/app.js
@@ -0,0 +1,13 @@
+var ta;
+var btn;
+window.onload = function() {
+    ta = document.getElementById("ta");
+    if (localStorage.text) {
+        ta.value = localStorage.text;
+    }
+    btn = document.getElementById("btn");
+    btn.onclick = function() {
+        // alert(ta.value);
+        localStorage.text = ta.value;
+    }
+}
